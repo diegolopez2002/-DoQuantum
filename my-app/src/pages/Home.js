79b883564e2fb688313f5b8224178da4799e7b2a
EvrenKissane <eyucekus@terpmail.umd.edu>
2024-12-23 16:39:42 -0500

    fixed

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
-
+import { useNavigate } from "react-router-dom";
 
 import Button from '../components/Button';
 import Login from '../components/Login';
