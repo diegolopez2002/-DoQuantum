fd604faf51a3296d46cce3ea7b0d7d9eeae5f9eb
EvrenKissane <eyucekus@terpmail.umd.edu>
2024-12-23 16:38:59 -0500

    added dahboard page after login

---
+++ my-app/src/pages/Dashboard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            color: '#ffffff',
+            fontFamily: '"Inter", system-ui, sans-serif',
+            }}>
+            <h1>Dashboard</h1>
+            <p>Welcome to the Dashboard!</p>
+        </div>
+    );
+}
+
+export default Dashboard;
