e652e62bf795e16c9537bf0ea35176e40bcfc837
EvrenKissane <eyucekus@terpmail.umd.edu>
2024-12-25 13:32:12 -0500

    added PORT value in hidden .env file

--- my-app/backend/Server.js
+++ my-app/backend/Server.js
@@ -8,7 +8,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const app = express();
-const port = process.env.PORT || 5000; // Backend will run on localhost:5000
+const port = process.env.PORT || 5000;
 
 const session = require('express-session');
 const passport = require('passport');
